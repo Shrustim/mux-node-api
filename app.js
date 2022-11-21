808369d79860a751af60c51650e5564c9254eddb
shrushti mane <shrustimanexyz@gmail.com>
2022-11-21 18:03:46 +0530

    minor change

--- app.js
+++ app.js
@@ -17,58 +17,39 @@ io.on('connection', (socket) => {
     socket.emit("FromAPI", { "abc": "kkkk" });
 
     socket.on('startStreaming', async(data) => {
-            console.log("key", data)
-            const rtmpUrl = `rtmps://global-live.mux.com/app/${data.key}`;
+        console.log("key", data)
+        const rtmpUrl = `rtmps://global-live.mux.com/app/${data.key}`;
 
-            global['ffmpeg'] = await child_process.spawn('ffmpeg', [
-                '-i', '-',
+        global['ffmpeg'] = await child_process.spawn('ffmpeg', [
+            '-i', '-',
 
-                // video codec config: low latency, adaptive bitrate
-                '-c:v', 'libx264', '-preset', 'veryfast', '-tune', 'zerolatency',
+            // video codec config: low latency, adaptive bitrate
+            '-c:v', 'libx264', '-preset', 'veryfast', '-tune', 'zerolatency',
 
-                // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
-                '-c:a', 'aac', '-strict', '-2', '-ar', '44100', '-b:a', '64k',
+            // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+            '-c:a', 'aac', '-strict', '-2', '-ar', '44100', '-b:a', '64k',
 
-                //force to overwrite
-                '-y',
+            //force to overwrite
+            '-y',
 
-                // used for audio sync
-                '-use_wallclock_as_timestamps', '1',
-                '-async', '1',
+            // used for audio sync
+            '-use_wallclock_as_timestamps', '1',
+            '-async', '1',
 
-                //'-filter_complex', 'aresample=44100', // resample audio to 44100Hz, needed if input is not 44100
-                //'-strict', 'experimental',
-                '-bufsize', '1000',
-                '-f', 'flv',
+            //'-filter_complex', 'aresample=44100', // resample audio to 44100Hz, needed if input is not 44100
+            //'-strict', 'experimental',
+            '-bufsize', '1000',
+            '-f', 'flv',
 
-                rtmpUrl
-            ]);
+            rtmpUrl
+        ]);
 
-            console.log(" ---------------- global['ffmpeg']", global['ffmpeg'])
-            socket.emit("ffmpegpass", global['ffmpeg']);
-        })
-        //     // Kill the WebSocket connection if ffmpeg dies.
-        // ffmpeg.on('close', (code, signal) => {
-        //     console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
+        console.log(" ---------------- global['ffmpeg']", global['ffmpeg'])
+        socket.emit("ffmpegpass", global['ffmpeg']);
+    })
 
-    //     socket.on('terminate', function() {
-    //         console.log('user terminate');
-    //     });
 
-    // });
 
-    // // Handle STDIN pipe errors by logging to the console.
-    // // These errors most commonly occur when FFmpeg closes and there is still
-    // // data to write.f If left unhandled, the server will crash.
-    // ffmpeg.stdin.on('error', (e) => {
-    //     console.log('FFmpeg STDIN Error', e);
-    // });
-
-    // // FFmpeg outputs all of its messages to STDERR. Let's log them to the console.
-    // ffmpeg.stderr.on('data', (data) => {
-    //     // ws.send('ffmpeg got some data');
-    //     console.log('FFmpeg STDERR:', data.toString());
-    // });
 
     socket.on('passDatatoStreaming', async(msg) => {
         // console.log("  global['ffmpeg']", global['ffmpeg'])
